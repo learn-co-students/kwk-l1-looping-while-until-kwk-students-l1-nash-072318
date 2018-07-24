b5fb623f09a72516765508f4f25cca98f15b7702
judy2254 <judyjaden225@icloud.com>
2018-07-24 19:54:34 +0000

    Done.

---
+++ magic.rb
@@ -0,0 +1,19 @@
+def dance 
+  moves_completed=6
+  while moves_completed<11
+  puts "Wingardium Leviosa"
+  #puts "#{moves_completed}"
+  moves_completed+=1 
+end #for while
+end #for method 
+dance
+
+
+
+
+
+
+
+
+
+
